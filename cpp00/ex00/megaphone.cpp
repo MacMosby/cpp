88f379f1ac8e511ac1025c4d16ca91e213ef9320
Marc Rodenbusch <mrodenbu@student.42berlin.de>
2024-10-04 21:06:17 +0200

    upgrade to more c++ way

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -17,8 +17,33 @@ stuf */
 
 #include <iostream>
 
-int main()
+void	scream(char *str)
 {
-	std::cout << "Hello World!\n";
+	int	i = 0;
+	char c;
+
+	while (str[i])
+	{
+		c = toupper(str[i]);
+		std::cout << c;
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i = 1;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (argv[i])
+	{
+		scream(argv[i]);
+		i++;
+	}
+	std::cout << std::endl;
 	return (0);
 }
