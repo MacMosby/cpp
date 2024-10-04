a75aa933f924bbe15c1dd4befa403513f4a9ea55
Marc Rodenbusch <mrodenbu@student.42berlin.de>
2024-10-04 18:56:41 +0200

    first cpp ever

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrodenbu <mrodenbu@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 16:18:25 by mrodenbu          #+#    #+#             */
+/*   Updated: 2024/10/04 16:18:27 by mrodenbu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+/* Namespaces, classes, member functions, stdio streams,
+initialization lists, static, const, and some other basic
+stuf */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	return (0);
+}
