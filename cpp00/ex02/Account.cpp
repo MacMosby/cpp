dfffd69010a12e243cf9946a134c3da1f1a67711
Marc Rodenbusch <mrodenbu@student.42berlin.de>
2024-10-15 20:03:12 +0200

    initialized ex02

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrodenbu <mrodenbu@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 18:38:41 by mrodenbu          #+#    #+#             */
+/*   Updated: 2024/10/15 18:38:42 by mrodenbu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include "iostream"
+
+Account::Account( void )
+{}
+
+Account::Account( int initial_deposit )
+{}
+
+Account::~Account( void )
+{}
+
+void	Account::makeDeposit( int deposit )
+{}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{}
+
+int		Account::checkAmount( void ) const
+{}
+
+void	Account::displayStatus(void) const
+{}
+
+static int	getNbAccounts( void )
+{}
+
+static int	getTotalAmount( void )
+{}
+
+static int	getNbDeposits( void )
+{}
+
+static int	getNbWithdrawals( void )
+{}
+
+static void	displayAccountsInfos( void )
+{}
+
+static void	_displayTimestamp( void )
+{}
