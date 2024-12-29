de9339b3e6419bbf80e38f54d1bbb3444c045aae
Marc Rodenbusch <mrodenbu@student.42berlin.de>
2024-12-29 13:25:21 +0100

    improved accessibility

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -18,21 +18,24 @@
 class PhoneBook
 {
 
-public:
+	public:
 
-	Contact	contacts[8];
+		PhoneBook(void);
+		~PhoneBook(void);
 
-	PhoneBook(void);
-	~PhoneBook(void);
+		void	add(void);
+		void	search(void) const;
 
-	void	add(void);
-	void	place_in_phonebook(Contact entry);
-	void	replace_oldest_entry(Contact new_entry);
-	void	search(void) const;
-	void	display_all_entries(void) const;
-	void	display_contact_line(Contact entry, int index) const;
-	void	display_contact_detail(std::string contact_detail) const;
-	void	display_contact(int index) const;
+	private:
+
+		Contact	contacts[8];
+
+		void	place_in_phonebook(Contact entry);
+		void	replace_oldest_entry(Contact new_entry);
+		void	display_all_entries(void) const;
+		void	display_contact_line(Contact entry, int index) const;
+		void	display_contact_detail(std::string contact_detail) const;
+		void	display_contact(int index) const;
 };
 
 #endif
