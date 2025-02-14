ed2ea0cce89130a18d89fe2ca2983c7742843bda
Marc Rodenbusch <mrodenbu@student.42berlin.de>
2025-02-14 19:48:41 +0100

    init cpp05 and its ex00

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrodenbu <mrodenbu@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 19:43:31 by mrodenbu          #+#    #+#             */
+/*   Updated: 2025/02/14 19:43:33 by mrodenbu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_H
+# define BUREAUCRAT_H
+
+# include <string>
+# include <iostream>
+
+class Bureaucrat
+{
+	public:
+
+	protected:
+
+	private:
+};
+
+#endif
